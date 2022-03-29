7e49b752f4e8d4b1e901edc9eb4f37fc3e12ef07
Shelby Drabant <drabants@gmail.com>
2022-03-29 14:05:13 -0700

    Updated Teleportation Controller script

--- Assets/Scripts/6 Teleportation/TeleportationController.cs
+++ Assets/Scripts/6 Teleportation/TeleportationController.cs
@@ -8,27 +8,37 @@ using UnityEngine.XR.Interaction.Toolkit;
 
 public class TeleportationController : MonoBehaviour
 {
+    //Used to determine current active state of the Teleportation System
+    static private bool _teleportIsActive = false;
+
     //Creates an enum that will determine if we're using the right or left controller
     public enum ControllerType
     {
         RightHand,
         LeftHand
     }
+
     //Stores the target controller from the editor
     public ControllerType targetController;
 
     //References our Input Actions that we are using
     public InputActionAsset inputAction;
+
     //References the rayInteractor to be enabled/disabled later
     public XRRayInteractor rayInteractor;
+
     //References the Teleportation Provider so we can use it to teleport the Player in the event of a succesful teleport call
     public TeleportationProvider teleportationProvider;
 
 
     //Will reference the Thumbstick Input Action when the scene starts up
     private InputAction _thumbstickInputAction;
-    //Used to determine current active state of the rayInteracor
-    private bool _isActive;
+
+    //Stores Action for Teleport Mode Activate
+    private InputAction _teleportActivate;
+
+    //Stores Action for Teleport Mode Cancel
+    private InputAction _teleportCancel;
 
     void Start()
     {
@@ -37,30 +47,41 @@ public class TeleportationController : MonoBehaviour
 
         //This will find the Action Map of our target controller for Teleport Mode Activate.
         //It will enable it and then subscribe itself to our OnTeleportActivate function
-        InputAction teleportActivate = inputAction.FindActionMap("XRI" + targetController.ToString()).FindAction("Teleport Mode Activate");
-        teleportActivate.Enable();
-        teleportActivate.performed += OnTeleportActivate;
+        Debug.Log("XRI " + targetController.ToString());
+        _teleportActivate = inputAction.FindActionMap("XRI " + targetController.ToString()).FindAction("Teleport Mode Activate");
+        _teleportActivate.Enable();
+        _teleportActivate.performed += OnTeleportActivate;
 
         //This will find the Action Map of our target controller for Teleport Mode Cancel.
         //It will enable it and then subscribe itself to our OnTeleportCancel function
-        InputAction teleportCancel = inputAction.FindActionMap("XRI" + targetController.ToString()).FindAction("Teleport Mode Cancel");
-        teleportCancel.Enable();
-        teleportCancel.performed += OnTeleportCancel;
+        _teleportCancel = inputAction.FindActionMap("XRI " + targetController.ToString()).FindAction("Teleport Mode Cancel");
+        _teleportCancel.Enable();
+        _teleportCancel.performed += OnTeleportCancel;
 
 
         //We grab this reference so we can use it to tell if the thumbstick is still being pressed
-        _thumbstickInputAction = inputAction.FindActionMap("XRI" + targetController.ToString()).FindAction("Move");
+        _thumbstickInputAction = inputAction.FindActionMap("XRI " + targetController.ToString()).FindAction("Move");
     }
 
+    private void OnDestroy()
+    {
+        _teleportActivate.performed -= OnTeleportActivate;
+        _teleportCancel.performed -= OnTeleportCancel;
+    }
     //We use the Update function to check for when a teleportation event has occured. 
     //The checks needed to ensure a succesful teleport event are
     //-Teleporting is currently active
+    //-The ray currently the active one
     //-The Thumbstick isn't being pressed
     //-The rayInteractor is hitting a valid target
     //If those pass, we make a teleportation request to the Teleport Provider
     void Update()
     {
-        if (!_isActive)
+        if (!_teleportIsActive)
+        {
+            return;
+        }
+        if (!rayInteractor.enabled)
         {
             return;
         }
@@ -70,11 +91,13 @@ public class TeleportationController : MonoBehaviour
         }
         if (!rayInteractor.TryGetCurrent3DRaycastHit(out RaycastHit raycastHit))
         {
+            Debug.Log(raycastHit);
             rayInteractor.enabled = false;
-            _isActive = false;
+            _teleportIsActive = false;
             return;
         }
 
+        Debug.Log(raycastHit);
         TeleportRequest teleportRequest = new TeleportRequest()
         {
             destinationPosition = raycastHit.point,
@@ -82,19 +105,29 @@ public class TeleportationController : MonoBehaviour
 
         teleportationProvider.QueueTeleportRequest(teleportRequest);
 
+        rayInteractor.enabled = false;
+        _teleportIsActive = false;
     }
 
     //This is called when our Teleport Mode Activated action map is triggered
     private void OnTeleportActivate(InputAction.CallbackContext context)
     {
-        rayInteractor.enabled = true;
-        _isActive = true;
+        if (!_teleportIsActive)
+        {
+            rayInteractor.enabled = true;
+            _teleportIsActive = true;
+        }
+
     }
 
     //This is called when our Teleport Mode Cancel action map is triggered
     private void OnTeleportCancel(InputAction.CallbackContext context)
     {
-        rayInteractor.enabled = false;
-        _isActive = false;
+        if (_teleportIsActive && rayInteractor.enabled == true)
+        {
+            rayInteractor.enabled = false;
+            _teleportIsActive = false;
+        }
+
     }
 }
