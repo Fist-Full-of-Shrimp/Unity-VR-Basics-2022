f0ae4cae898fb1e4ccaa467c69eafb653919f81d
Shelby Drabant <drabants@gmail.com>
2022-05-23 00:43:46 -0700

    fixing my whooopsie

--- Assets/Scripts/11 Wrist UI/WristUI.cs
+++ Assets/Scripts/11 Wrist UI/WristUI.cs
@@ -1,16 +1,25 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.InputSystem;
 public class WristUI : MonoBehaviour
 {
-    private Canvas _wristUICanvas;
+    public InputActionAsset inputActions;
 
+    private Canvas _wristUICanvas;
+    private InputAction _menu;
     private void Start()
     {
         _wristUICanvas = GetComponent<Canvas>();
+        _menu = inputActions.FindActionMap("XRI LeftHand").FindAction("Menu");
+        _menu.Enable();
+        _menu.performed += ToggleMenu;
     }
-    public void ToggleMenu()
+
+    private void OnDestroy()
+    {
+        _menu.performed -= ToggleMenu;
+    }
+
+    public void ToggleMenu(InputAction.CallbackContext context)
     {
         _wristUICanvas.enabled = !_wristUICanvas.enabled;
     }
